118aadb136a02f61b38c2ae22f3d443097a56772
Steven Sim <stevensim226@gmail.com>
2021-08-08 20:05:27 +0700

    Add utility test

---
+++ spec/utility/json_response_spec.rb
@@ -0,0 +1,14 @@
+load './app/utility/json_response.rb'
+
+OBJ = {"Hello" => "World"}
+SUCCESS_STATUS = false
+
+RSpec.describe "Comments", type: :request do
+    context "Test methods" do
+        it "Make sure build_response method is working" do
+            result = build_response(OBJ, SUCCESS_STATUS)
+            expect(result[:success]).to eq(SUCCESS_STATUS)
+            expect(result[:content]).to eq(OBJ)
+        end
+    end
+end
