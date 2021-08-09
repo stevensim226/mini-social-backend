81f75ced8c61f14b72aefdde3b24a20f90891bf9
Steven Sim <stevensim226@gmail.com>
2021-08-09 22:34:03 +0700

    Fix script

--- .circleci/deploy_ec2.sh
+++ .circleci/deploy_ec2.sh
@@ -1,7 +1,7 @@
-sudo docker pull stevensim226/minisocial:latest
+sudo docker pull stevensim226/minisocial-be:latest
 #sudo docker container rm minisocial-app
 sudo docker run --name minisocial-app \
-    -e MINISOCIAL_DATABASE_HOST=$MINISOCIAL_DATABASE_HOST
-    -e MINISOCIAL_DATABASE_PASSWORD=$MINISOCIAL_DATABASE_PASSWORD
-    -p 80:3000
-    -d stevensim226/minisocial:latest
+    -e MINISOCIAL_DATABASE_HOST=$MINISOCIAL_DATABASE_HOST \
+    -e MINISOCIAL_DATABASE_PASSWORD=$MINISOCIAL_DATABASE_PASSWORD \
+    -p 80:3000 \
+    -d stevensim226/minisocial-be:latest
