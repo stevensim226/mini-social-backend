151f1d6c77f652c115ecb767d190a7c752c52ae9
Steven <57280118+stevensim226@users.noreply.github.com>
2021-08-09 23:48:57 +0700

    Add dockerfile, pipeline, and deployment shell script (#9)
    * Add dockerfile, pipeline, and deployment shell script
    
    * Remove master branch only filter temp
    
    * Fix dockerfile
    
    * Quickfix config
    
    * Quickfix config
    
    * Fix dockerfile again
    
    * Fix script
    
    * Fix pipeline
    
    * Fix wrong file
    
    * fix wrong file
    
    * Add fingerprint settings
    
    * Fix script
    
    * Add master key config setup
    
    * Uncomment rm
    
    * Add prod script
    
    * Cleanup
    
    * update script
    
    * Last test
    
    * Edit dockerfile
    
    * Edit dockerfile
    
    * Fix script
    
    * [SKIP CI] Finalize pipeline

---
+++ .circleci/deploy_ec2.sh
@@ -0,0 +1,9 @@
+sudo docker pull stevensim226/minisocial-be:latest
+sudo docker rm -f minisocial-app
+sudo docker run --name minisocial-app \
+    -e MINISOCIAL_DATABASE_HOST=$MINISOCIAL_DATABASE_HOST \
+    -e MINISOCIAL_DATABASE_PASSWORD=$MINISOCIAL_DATABASE_PASSWORD \
+    -e MASTER_KEY=$MASTER_KEY \
+    -p 80:3000 \
+    -d stevensim226/minisocial-be:latest
+exit
---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM ruby:3.0.0
+
+EXPOSE 3000
+
+RUN mkdir rubyapp/
+WORKDIR rubyapp/
+
+ADD . .
+
+RUN bundle install
+
+CMD ["bash", "run_prod.sh"]
