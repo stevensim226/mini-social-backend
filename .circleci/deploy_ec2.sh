74b84b488588f734f7383c0d5d733f6749b5096f
Steven Sim <stevensim226@gmail.com>
2021-08-09 22:56:47 +0700

    Uncomment rm

--- .circleci/deploy_ec2.sh
+++ .circleci/deploy_ec2.sh
@@ -1,5 +1,5 @@
 sudo docker pull stevensim226/minisocial-be:latest
-#sudo docker container rm minisocial-app
+sudo docker container rm minisocial-app
 sudo docker run --name minisocial-app \
     -e MINISOCIAL_DATABASE_HOST=$MINISOCIAL_DATABASE_HOST \
     -e MINISOCIAL_DATABASE_PASSWORD=$MINISOCIAL_DATABASE_PASSWORD \
