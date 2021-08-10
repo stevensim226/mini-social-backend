6f4f2e6a8c08bda665e3db1720b30d6cfba8cf86
Steven <57280118+stevensim226@users.noreply.github.com>
2021-08-10 20:37:50 +0700

    Refactor pipeline (#10)
    * Refactor pipeline
    
    * Try no ssh key manual
    
    * Cleanup pipeline script

--- .circleci/deploy_ec2.sh
+++ .circleci/deploy_ec2.sh
@@ -4,6 +4,6 @@ sudo docker run --name minisocial-app \
     -e MINISOCIAL_DATABASE_HOST=$MINISOCIAL_DATABASE_HOST \
     -e MINISOCIAL_DATABASE_PASSWORD=$MINISOCIAL_DATABASE_PASSWORD \
     -e MASTER_KEY=$MASTER_KEY \
-    -p 80:3000 \
+    -p 3000:3000 \
     -d stevensim226/minisocial-be:latest
 exit
